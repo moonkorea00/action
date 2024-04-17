8e5ff5bd3b1a46fe6ea3b557c5d194faea1ce670
moonkorea00 <moonkorea00@gmail.com>
2024-04-18 04:54:45 +0900

    release commit

---
+++ index.ts
@@ -0,0 +1,17 @@
+import github from '@actions/github';
+import core from '@actions/core';
+
+async function run() {
+  const context = github.context;
+
+  if (context.eventName === 'pull_request' && ['opened', 'reopened', 'synchronize'].includes(context.payload.action!)) {
+    // Handle PR open/reopen/synchronize
+    console.log('PR opened, reopened, or code pushed');
+  } else if (context.eventName === 'pull_request_target' && context.payload.pull_request!.merged) {
+    // Handle PR merged
+    console.log('PR merged');
+    // You could potentially manage creating/updating issues here
+  }
+}
+
+run().catch(err => core.setFailed(`Action failed with error ${err}`));
