6ad4c0519adf2b711d7be7ff12dd5519d84456a6
moonkorea00 <moonkorea00@gmail.com>
2024-04-19 13:38:19 +0900

    comment

---
+++ src/comment.js
@@ -0,0 +1,82 @@
+const createPullRequestComment = async (octokit, context, commentBody) => {
+  const commentId = `lighthouse-report-tracker-${context.payload.pull_request.number}`;
+  const comments = await octokit.rest.issues.listComments({
+    owner: context.repo.owner,
+    repo: context.repo.repo,
+    issue_number: context.payload.pull_request.number,
+  });
+  console.log('THIS IS COMMENTS : ', comments);
+
+  // const comment = comments.data.find(
+  //   c =>
+  //     c.user.login === 'your-bot-username' &&
+  //     c.body.startsWith('Specific identifier or content')
+  // );
+  // const commentId = comment.id;
+
+  // const mutateComment = comments.find(
+  //   comment => comment.comment_id === commentId
+  // )
+  //   ? octokit.rest.issues.updateComment
+  //   : octokit.rest.issues.createComment;
+
+  // return mutateComment({
+  //   owner: context.repo.owner,
+  //   repo: context.repo.repo,
+  //   issue_number: context.payload.pull_request.number,
+  //   comment_id: commentId,
+  //   body: commentBody,
+  // });
+  return octokit.rest.issues.createComment({
+    owner: context.repo.owner,
+    repo: context.repo.repo,
+    issue_number: context.payload.pull_request.number,
+    body: commentBody,
+  });
+};
+
+const formatMetricValueDifference = (curr, prev) => {
+  if (prev === '➖') return '➖';
+  const diff = prev - curr;
+  return `${
+    diff === 0 ? '➖' : diff > 0 ? '🔻' + -diff : '🔺' + Math.abs(diff)
+  }`;
+};
+
+const createReportComparisonTable = (
+  context,
+  currentReports,
+  previousReports
+) => {
+  let commentBody = `### Lighthouse Report\n\n`;
+
+  currentReports.forEach(currReport => {
+    const prevReport = previousReports?.find(
+      prevReport => prevReport.url === currReport.url
+    );
+
+    const tableHeading = `#### ${currReport.url} \n`;
+    const rowHeader = `| Metric | Previous Score${
+      prevReport && `(#${prevReport.pr})`
+    } | Current Score(#${
+      context.payload.pull_request.number
+    }) | Difference |\n`;
+    const seperator = `|:------:|:----------------:|:-----------------:|:----------:|\n`;
+
+    let baseTable = tableHeading + rowHeader + seperator;
+
+    Object.keys(currReport.summary).forEach(metric => {
+      const currValue = currReport.summary[metric] * 100;
+      const prevValue = prevReport ? prevReport.summary[metric] * 100 : '➖';
+      const difference = formatMetricValueDifference(currValue, prevValue);
+
+      baseTable += `| ${metric} | ${prevValue} | ${currValue} | ${difference} |\n`;
+    });
+
+    commentBody += baseTable + '\n\n';
+  });
+
+  return baseTable;
+};
+
+module.export = { createPullRequestComment, createReportComparisonTable };
---
+++ src/index.js
@@ -0,0 +1,71 @@
+const github = require('@actions/github');
+const core = require('@actions/core');
+
+const {
+  createPullRequestComment,
+  createReportComparisonTable,
+} = require('./comment');
+
+async function main() {
+  try {
+    const context = github.context;
+    const token = core.getInput('token');
+    const octokit = github.getOctokit(token);
+
+    if (
+      context.eventName === 'pull_request' &&
+      ['opened', 'reopened', 'synchronize'].includes(context.payload.action)
+    ) {
+      core.info('Start running lighthouse report tracker v1.0.0..');
+      // const outputDir = core.getInput('outputDir')
+      // const summary = JSON.parse(fs.readFileSync(`${outputDir}/manifest.json`));
+
+      // const previousReports = issues.find(issue => issue.id === `lighthouse-report-log`) // update using github api
+      const currentReports = JSON.parse(
+        JSON.stringify([
+          {
+            url: 'http://localhost:3000/',
+            pr: 15,
+            summary: {
+              performance: 0.88,
+              accessibility: 0.74,
+              'best-practices': 0.81,
+              seo: 0.92,
+              pwa: 0.88,
+            },
+          },
+          {
+            url: 'http://localhost:3000/post',
+            pr: 13,
+            summary: {
+              performance: 0.88,
+              accessibility: 0.74,
+              'best-practices': 0.81,
+              seo: 0.92,
+              pwa: 0.88,
+            },
+          },
+        ])
+      );
+
+      const commentBody = createReportComparisonTable(
+        context,
+        currentReports,
+        (previousReports = [])
+      );
+      await createPullRequestComment(octokit, context, commentBody);
+    } else if (
+      context.eventName === 'pull_request_target' &&
+      context.payload.pull_request.merged
+    ) {
+      // createOrUpdateIssue();
+    }
+  } catch (err) {
+    core.setFailed(`Failed running action with error : ${err}`);
+  } finally {
+    core.info('End running lighthouse report tracker v1.0.0..');
+  }
+}
+
+main();
+// main().catch(err => core.setFailed(`Action failed with error ${err}`)).finally;
